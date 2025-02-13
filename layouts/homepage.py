57be50693633584a2bc6ba468ddd38f8412810c6
Andrew B <andrew.ballard@ufl.edu>
2025-02-13 15:56:54 -0500

    updated hompage and sidebar

--- layouts/homepage.py
+++ layouts/homepage.py
@@ -1,10 +1,7 @@
-from dash import html, dcc
+from dash import html
 import dash_bootstrap_components as dbc
 
 homepage_layout = dbc.Container([
     dbc.Row(dbc.Col(html.H1("Homepage", className="text-center mt-4"))),
     dbc.Row(dbc.Col(html.P("Welcome to the Homepage!", className="text-center"))),
-    dbc.Row(dbc.Col(dcc.Link('Go to Login', href='/login', className="btn btn-primary btn-lg d-block mx-auto my-2", style={"width": "200px"}))),
-    dbc.Row(dbc.Col(dcc.Link('Go to Register', href='/register', className="btn btn-secondary btn-lg d-block mx-auto my-2", style={"width": "200px"}))),
-    dbc.Row(dbc.Col(dcc.Link('Go to Dashboard', href='/dashboard', className="btn btn-info btn-lg d-block mx-auto my-2", style={"width": "200px"}))),
 ], fluid=True)
